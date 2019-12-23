105fc842e92aecbbe1304b388858ee7ce3124e31
WaltRibeiro <walt@fororchestra.com>
2019-12-23 15:46:44 -0500

    commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var number1 = 100
+var number2 = 100
+
+for (var number1; number1 < 1999; number1++) {
+    if number1 * number2 = (i " is divisible by 3");
+	else (i % 5 == 0) console.log(i " is divisible by 5");
+}
