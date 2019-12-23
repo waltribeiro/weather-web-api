98acfbb2c9ef763994fa7f7d21b795c8a7176d52
WaltRibeiro <walt@fororchestra.com>
2019-12-23 00:00:43 -0500

    commit

--- python.py
+++ python.py
@@ -1,7 +1,13 @@
-//machine learning algorithm goes here
+#machine learning algorithm goes here
 
 def compact(lst):
     return list(filter(None, lst))
   
   
 compact([0, 1, False, 2, '', 3, 'a', 's', 34]) # [ 1, 2, 3, 'a', 's', 34 ]
+
+#transpose 2d array
+
+array = [['a', 'b'], ['c', 'd'], ['e', 'f']]
+transposed = zip(*array)
+print(transposed) # [('a', 'c', 'e'), ('b', 'd', 'f')]
